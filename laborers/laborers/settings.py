fb1abede7fe1e94fe988e84c35609784b62c43fc
Dagmawi Tewodros <dagmawialebachew@gmail.com>
2025-03-16 16:20:55 +0300

    change module name wsgi

--- laborers/laborers/settings.py
+++ laborers/laborers/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'payease']
+    'payease'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
