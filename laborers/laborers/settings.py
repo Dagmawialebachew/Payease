bfea537aba0e8106acbf20e3ed4a82acd37650dd
Dagmawi Tewodros <dagmawialebachew@gmail.com>
2025-03-16 15:44:49 +0300

    Initial commit

--- laborers/laborers/settings.py
+++ laborers/laborers/settings.py
@@ -45,9 +45,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lomln!+z#pg&fybrrvpq+qw^9gfi0ktbh)g4rl9_mlm26m8_dt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -158,6 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
         os.path.join(BASE_DIR, "static/"),
